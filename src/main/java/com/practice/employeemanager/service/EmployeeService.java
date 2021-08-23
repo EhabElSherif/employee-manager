9952e881c159b5ef78ac0c81976bfc33cac967e9
Ehab El-Sherif <ehabraelsherif@gmail.com>
2021-08-23 22:53:59 +0200

    complete employee-service

--- src/main/java/com/practice/employeemanager/service/EmployeeService.java
+++ src/main/java/com/practice/employeemanager/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.practice.employeemanager.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -20,4 +21,22 @@ public class EmployeeService {
         employee.setEmployee_code(UUID.randomUUID().toString());
         return employeeRepository.save(employee);
     }
+
+    public List<Employee> findAllEmployee(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id){
+        boolean exists = employeeRepository.existsById(id);
+        if(!exists){
+            throw new IllegalStateException("employee with id "+id+" doesn't exists");
+        }
+        employeeRepository.deleteById(id);
+    }
+
+
 }
